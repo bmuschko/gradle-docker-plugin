57af9ddf9684c3522958ce17866cfafff78be771
Benjamin Muschko <benjamin.muschko@gmail.com>
2014-05-08 20:21:49 -0400

    Added more tasks

---
+++ src/main/groovy/org/gradle/api/plugins/docker/tasks/DockerVersion.groovy
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gradle.api.plugins.docker.tasks
+
+class DockerVersion extends AbstractDockerTask {
+    @Override
+    void runRemoteCommand(URLClassLoader classLoader) {
+        logger.quiet "Retrieving Docker version."
+        def dockerClient = getDockerClient(classLoader)
+        def version = dockerClient.version()
+        logger.quiet "Version          : $version.version"
+        logger.quiet "Git Commit       : $version.gitCommit"
+        logger.quiet "Go Version       : $version.goVersion"
+        logger.quiet "Kernel Version   : $version.kernelVersion"
+        logger.quiet "Architecture     : $version.arch"
+        logger.quiet "Operating System : $version.operatingSystem"
+    }
+}
