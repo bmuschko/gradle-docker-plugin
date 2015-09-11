c43468a66f82af31a9c33900866504348a1a2e73
Benjamin Muschko <benjamin.muschko@gmail.com>
2015-09-11 08:30:51 -0400

    Better formatting.

--- src/functTest/groovy/com/bmuschko/gradle/docker/DockerWorkflowFunctionalTest.groovy
+++ src/functTest/groovy/com/bmuschko/gradle/docker/DockerWorkflowFunctionalTest.groovy
@@ -22,31 +22,32 @@ import spock.lang.Requires
 class DockerWorkflowFunctionalTest extends AbstractFunctionalTest {
     def "Can create Dockerfile and build an image from it"() {
         buildFile << """
-import com.bmuschko.gradle.docker.tasks.image.Dockerfile
-import com.bmuschko.gradle.docker.tasks.image.DockerBuildImage
-import com.bmuschko.gradle.docker.tasks.image.DockerInspectImage
+            import com.bmuschko.gradle.docker.tasks.image.Dockerfile
+            import com.bmuschko.gradle.docker.tasks.image.DockerBuildImage
+            import com.bmuschko.gradle.docker.tasks.image.DockerInspectImage
 
-task createDockerfile(type: Dockerfile) {
-    destFile = project.file('build/mydockerfile/Dockerfile')
-    from 'ubuntu:12.04'
-    maintainer 'Benjamin Muschko "benjamin.muschko@gmail.com"'
-}
+            task createDockerfile(type: Dockerfile) {
+                destFile = project.file('build/mydockerfile/Dockerfile')
+                from 'ubuntu:12.04'
+                maintainer 'Benjamin Muschko "benjamin.muschko@gmail.com"'
+            }
 
-task buildImage(type: DockerBuildImage) {
-    dependsOn createDockerfile
-    inputDir = createDockerfile.destFile.parentFile
-    tag = "${createUniqueImageId()}"
-}
+            task buildImage(type: DockerBuildImage) {
+                dependsOn createDockerfile
+                inputDir = createDockerfile.destFile.parentFile
+                tag = "${createUniqueImageId()}"
+            }
 
-task inspectImage(type: DockerInspectImage) {
-    dependsOn buildImage
-    targetImageId { buildImage.getImageId() }
-}
+            task inspectImage(type: DockerInspectImage) {
+                dependsOn buildImage
+                targetImageId { buildImage.getImageId() }
+            }
 
-task workflow {
-    dependsOn inspectImage
-}
-"""
+            task workflow {
+                dependsOn inspectImage
+            }
+        """
+
         when:
         BuildResult result = build('workflow')
 
@@ -60,23 +61,24 @@ task workflow {
         createDockerfile(imageDir)
 
         buildFile << """
-import com.bmuschko.gradle.docker.tasks.image.DockerBuildImage
-import com.bmuschko.gradle.docker.tasks.image.DockerInspectImage
+            import com.bmuschko.gradle.docker.tasks.image.DockerBuildImage
+            import com.bmuschko.gradle.docker.tasks.image.DockerInspectImage
 
-task buildImage(type: DockerBuildImage) {
-    inputDir = file('images/minimal')
-    tag = "${createUniqueImageId()}"
-}
+            task buildImage(type: DockerBuildImage) {
+                inputDir = file('images/minimal')
+                tag = "${createUniqueImageId()}"
+            }
 
-task inspectImage(type: DockerInspectImage) {
-    dependsOn buildImage
-    targetImageId { buildImage.getImageId() }
-}
+            task inspectImage(type: DockerInspectImage) {
+                dependsOn buildImage
+                targetImageId { buildImage.getImageId() }
+            }
 
-task workflow {
-    dependsOn inspectImage
-}
-"""
+            task workflow {
+                dependsOn inspectImage
+            }
+        """
+
         when:
         BuildResult result = build('workflow')
 
@@ -91,43 +93,44 @@ task workflow {
         String uniqueContainerName = createUniqueContainerName()
 
         buildFile << """
-import com.bmuschko.gradle.docker.tasks.image.DockerBuildImage
-import com.bmuschko.gradle.docker.tasks.container.DockerCreateContainer
-import com.bmuschko.gradle.docker.tasks.container.DockerStartContainer
-import com.bmuschko.gradle.docker.tasks.container.DockerInspectContainer
-import com.bmuschko.gradle.docker.tasks.container.DockerKillContainer
+            import com.bmuschko.gradle.docker.tasks.image.DockerBuildImage
+            import com.bmuschko.gradle.docker.tasks.container.DockerCreateContainer
+            import com.bmuschko.gradle.docker.tasks.container.DockerStartContainer
+            import com.bmuschko.gradle.docker.tasks.container.DockerInspectContainer
+            import com.bmuschko.gradle.docker.tasks.container.DockerKillContainer
 
-task buildImage(type: DockerBuildImage) {
-    inputDir = file('images/minimal')
-    tag = "${createUniqueImageId()}"
-}
+            task buildImage(type: DockerBuildImage) {
+                inputDir = file('images/minimal')
+                tag = "${createUniqueImageId()}"
+            }
 
-task createContainer(type: DockerCreateContainer) {
-    dependsOn buildImage
-    targetImageId { buildImage.getImageId() }
-    containerName = "$uniqueContainerName"
-    portBindings = ['8080:8080']
-}
+            task createContainer(type: DockerCreateContainer) {
+                dependsOn buildImage
+                targetImageId { buildImage.getImageId() }
+                containerName = "$uniqueContainerName"
+                portBindings = ['8080:8080']
+            }
 
-task startContainer(type: DockerStartContainer) {
-    dependsOn createContainer
-    targetContainerId { createContainer.getContainerId() }
-}
+            task startContainer(type: DockerStartContainer) {
+                dependsOn createContainer
+                targetContainerId { createContainer.getContainerId() }
+            }
 
-task inspectContainer(type: DockerInspectContainer) {
-    dependsOn startContainer
-    targetContainerId { startContainer.getContainerId() }
-}
+            task inspectContainer(type: DockerInspectContainer) {
+                dependsOn startContainer
+                targetContainerId { startContainer.getContainerId() }
+            }
 
-task killContainer(type: DockerKillContainer) {
-    dependsOn inspectContainer
-    targetContainerId { startContainer.getContainerId() }
-}
+            task killContainer(type: DockerKillContainer) {
+                dependsOn inspectContainer
+                targetContainerId { startContainer.getContainerId() }
+            }
 
-task workflow {
-    dependsOn killContainer
-}
-"""
+            task workflow {
+                dependsOn killContainer
+            }
+        """
+
         expect:
         BuildResult result = build('workflow')
         result.standardOutput.contains("Name       : /$uniqueContainerName")
@@ -140,37 +143,38 @@ task workflow {
         String uniqueContainerName = createUniqueContainerName()
 
         buildFile << """
-import com.bmuschko.gradle.docker.tasks.image.DockerBuildImage
-import com.bmuschko.gradle.docker.tasks.container.DockerCreateContainer
-import com.bmuschko.gradle.docker.tasks.container.DockerInspectContainer
+            import com.bmuschko.gradle.docker.tasks.image.DockerBuildImage
+            import com.bmuschko.gradle.docker.tasks.container.DockerCreateContainer
+            import com.bmuschko.gradle.docker.tasks.container.DockerInspectContainer
 
-task buildImage(type: DockerBuildImage) {
-    inputDir = file('images/minimal')
-    tag = "${createUniqueImageId()}"
-}
+            task buildImage(type: DockerBuildImage) {
+                inputDir = file('images/minimal')
+                tag = "${createUniqueImageId()}"
+            }
 
-task createContainer1(type: DockerCreateContainer) {
-    dependsOn buildImage
-    targetImageId { buildImage.getImageId() }
-    containerName = "${uniqueContainerName}1"
-}
+            task createContainer1(type: DockerCreateContainer) {
+                dependsOn buildImage
+                targetImageId { buildImage.getImageId() }
+                containerName = "${uniqueContainerName}1"
+            }
 
-task createContainer2(type: DockerCreateContainer) {
-    dependsOn createContainer1
-    targetImageId { buildImage.getImageId() }
-    containerName = "${uniqueContainerName}2"
-    links = ["${uniqueContainerName}1:container1"]
-}
+            task createContainer2(type: DockerCreateContainer) {
+                dependsOn createContainer1
+                targetImageId { buildImage.getImageId() }
+                containerName = "${uniqueContainerName}2"
+                links = ["${uniqueContainerName}1:container1"]
+            }
 
-task inspectContainer(type: DockerInspectContainer) {
-    dependsOn createContainer2
-    targetContainerId { createContainer2.getContainerId() }
-}
+            task inspectContainer(type: DockerInspectContainer) {
+                dependsOn createContainer2
+                targetContainerId { createContainer2.getContainerId() }
+            }
 
-task workflow {
-    dependsOn inspectContainer
-}
-"""
+            task workflow {
+                dependsOn inspectContainer
+            }
+        """
+
         expect:
         BuildResult result = build('workflow')
         result.standardOutput.contains("Links      : [${uniqueContainerName}1:container1]")
@@ -179,44 +183,44 @@ task workflow {
     @Requires({ TestPrecondition.DOCKERHUB_CREDENTIALS_AVAILABLE })
     def "Can push image to DockerHub and pull it afterward"() {
         buildFile << """
-docker {
-    registryCredentials {
-        username = project.hasProperty('dockerHubUsername') ? project.property('dockerHubUsername') : null
-        password = project.hasProperty('dockerHubPassword') ? project.property('dockerHubPassword') : null
-        email = project.hasProperty('dockerHubEmail') ? project.property('dockerHubEmail') : null
-    }
-}
+            docker {
+                registryCredentials {
+                    username = project.hasProperty('dockerHubUsername') ? project.property('dockerHubUsername') : null
+                    password = project.hasProperty('dockerHubPassword') ? project.property('dockerHubPassword') : null
+                    email = project.hasProperty('dockerHubEmail') ? project.property('dockerHubEmail') : null
+                }
+            }
 
-import com.bmuschko.gradle.docker.tasks.container.DockerCreateContainer
-import com.bmuschko.gradle.docker.tasks.image.DockerCommitImage
-import com.bmuschko.gradle.docker.tasks.image.DockerPushImage
-import com.bmuschko.gradle.docker.tasks.image.DockerPullImage
+            import com.bmuschko.gradle.docker.tasks.container.DockerCreateContainer
+            import com.bmuschko.gradle.docker.tasks.image.DockerCommitImage
+            import com.bmuschko.gradle.docker.tasks.image.DockerPushImage
+            import com.bmuschko.gradle.docker.tasks.image.DockerPullImage
 
-task createContainer(type: DockerCreateContainer) {
-    imageId = 'busybox'
-    cmd = ['true'] as String[]
-}
+            task createContainer(type: DockerCreateContainer) {
+                imageId = 'busybox'
+                cmd = ['true'] as String[]
+            }
 
-task commitImage(type: DockerCommitImage) {
-    dependsOn createContainer
-    repository = "\$docker.registryCredentials.username/busybox"
-    targetContainerId { createContainer.getContainerId() }
-}
+            task commitImage(type: DockerCommitImage) {
+                dependsOn createContainer
+                repository = "\$docker.registryCredentials.username/busybox"
+                targetContainerId { createContainer.getContainerId() }
+            }
 
-task pushImage(type: DockerPushImage) {
-    dependsOn commitImage
-    imageName = "\$docker.registryCredentials.username/busybox"
-}
+            task pushImage(type: DockerPushImage) {
+                dependsOn commitImage
+                imageName = "\$docker.registryCredentials.username/busybox"
+            }
 
-task pullImage(type: DockerPullImage) {
-    dependsOn pushImage
-    repository = "\$docker.registryCredentials.username/busybox"
-}
+            task pullImage(type: DockerPullImage) {
+                dependsOn pushImage
+                repository = "\$docker.registryCredentials.username/busybox"
+            }
 
-task workflow {
-    dependsOn pullImage
-}
-"""
+            task workflow {
+                dependsOn pullImage
+            }
+        """
 
         expect:
         build('workflow')
@@ -225,31 +229,32 @@ task workflow {
     @Requires({ TestPrecondition.DOCKER_PRIVATE_REGISTRY_REACHABLE })
     def "Can build an image and push to private registry"() {
         buildFile << """
-import com.bmuschko.gradle.docker.tasks.image.Dockerfile
-import com.bmuschko.gradle.docker.tasks.image.DockerBuildImage
-import com.bmuschko.gradle.docker.tasks.image.DockerPushImage
+            import com.bmuschko.gradle.docker.tasks.image.Dockerfile
+            import com.bmuschko.gradle.docker.tasks.image.DockerBuildImage
+            import com.bmuschko.gradle.docker.tasks.image.DockerPushImage
 
-task createDockerfile(type: Dockerfile) {
-    destFile = project.file('build/mydockerfile/Dockerfile')
-    from 'ubuntu:12.04'
-    maintainer 'Benjamin Muschko "benjamin.muschko@gmail.com"'
-}
+            task createDockerfile(type: Dockerfile) {
+                destFile = project.file('build/mydockerfile/Dockerfile')
+                from 'ubuntu:12.04'
+                maintainer 'Benjamin Muschko "benjamin.muschko@gmail.com"'
+            }
 
-task buildImage(type: DockerBuildImage) {
-    dependsOn createDockerfile
-    inputDir = createDockerfile.destFile.parentFile
-    tag = '${TestConfiguration.dockerPrivateRegistryDomain}/${createUniqueImageId()}'
-}
+            task buildImage(type: DockerBuildImage) {
+                dependsOn createDockerfile
+                inputDir = createDockerfile.destFile.parentFile
+                tag = '${TestConfiguration.dockerPrivateRegistryDomain}/${createUniqueImageId()}'
+            }
 
-task pushImage(type: DockerPushImage) {
-    dependsOn buildImage
-    conventionMapping.imageName = { buildImage.getTag() }
-}
+            task pushImage(type: DockerPushImage) {
+                dependsOn buildImage
+                conventionMapping.imageName = { buildImage.getTag() }
+            }
 
-task workflow {
-    dependsOn pushImage
-}
-"""
+            task workflow {
+                dependsOn pushImage
+            }
+        """
+
         when:
         build('workflow')
 
