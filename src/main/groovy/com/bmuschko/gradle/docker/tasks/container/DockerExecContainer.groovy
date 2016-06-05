2290358cc378520683430e21b6ba0ae83cd85771
Christopher Dancy <dancc@pega.com>
2016-06-04 23:10:37 -0400

    ADDED: minor refactor and functional test for DockerExecContainer.

--- src/main/groovy/com/bmuschko/gradle/docker/tasks/container/DockerExecContainer.groovy
+++ src/main/groovy/com/bmuschko/gradle/docker/tasks/container/DockerExecContainer.groovy
@@ -16,18 +16,42 @@
 package com.bmuschko.gradle.docker.tasks.container
 
 import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.Optional
 
-class DockerExecute extends DockerExistingContainer {
+class DockerExecContainer extends DockerExistingContainer {
 
-  @Input
-  String[] cmd
+  	@Input
+  	String[] cmd
+  	
+  	@Input
+  	@Optional
+  	Boolean attachStdout = true
+  	
+  	@Input
+  	@Optional
+  	Boolean attachStderr = true
+  	
 
-  @Override
-  void runRemoteCommand(dockerClient) {
-    logger.quiet "Executing '${getCmd()}' on container with ID '${getContainerId()}'."
-    def execCallback = threadContextClassLoader.createExecCallback(System.out, System.err)
-    def execCmd = dockerClient.execCreateCmd(getContainerId()).withAttachStdout(true).withAttachStderr(true).withCmd(getCmd()).exec()
-    dockerClient.execStartCmd(execCmd.getId()).withDetach(false).exec(execCallback).awaitCompletion();  
-  }
-
+  	@Override
+	void runRemoteCommand(dockerClient) {
+  		logger.quiet "Executing '${getCmd()}' on container with ID '${getContainerId()}'."
+    	def execCallback = threadContextClassLoader.createExecCallback(System.out, System.err)
+    	def execCmd = dockerClient.execCreateCmd(getContainerId())
+    	setContainerCommandConfig(execCmd)
+    	dockerClient.execStartCmd(execCmd.exec().getId()).withDetach(false).exec(execCallback).awaitCompletion();  
+  	}
+  	
+  	private void setContainerCommandConfig(containerCommand) {  	
+  	    if (getCmd()) {
+  	    	containerCommand.withCmd(getCmd())
+  	    }
+  	
+        if (getAttachStderr()) {
+  	    	containerCommand.withAttachStderr(getAttachStderr())
+        }
+        
+        if (getAttachStdout()) {
+  	    	containerCommand.withAttachStdout(getAttachStdout())
+        }
+    }
 }
