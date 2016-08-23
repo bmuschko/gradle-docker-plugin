3fba40575eb0fd0ffadd508a1524231ef67a47ef
Mathieu JACQUES <mat.jacques@gmail.com>
2016-08-23 18:24:04 +0200

    Added support for force parameter on docker image removal

--- src/main/groovy/com/bmuschko/gradle/docker/tasks/image/DockerRemoveImage.groovy
+++ src/main/groovy/com/bmuschko/gradle/docker/tasks/image/DockerRemoveImage.groovy
@@ -15,10 +15,27 @@
  */
 package com.bmuschko.gradle.docker.tasks.image
 
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.Optional
+
 class DockerRemoveImage extends DockerExistingImage {
+
+	/**
+     * Forces removal.
+     */
+    @Input
+    @Optional
+    Boolean force
+
     @Override
     void runRemoteCommand(dockerClient) {
         logger.quiet "Removing image with ID '${getImageId()}'."
-        dockerClient.removeImageCmd(getImageId()).exec()
+        def removeImageCmd = dockerClient.removeImageCmd(getImageId())
+        
+        if(getForce()) {
+            removeImageCmd.withForce(getForce())
+        }
+        
+        removeImageCmd.exec()
     }
 }
