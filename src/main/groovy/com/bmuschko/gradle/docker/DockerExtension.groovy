60733723b5726ebe4f28e31807e0fd4c537f0dc1
Christopher Dancy <cdancy@users.noreply.github.com>
2016-12-19 18:25:38 -0500

    Update DockerExtension.groovy

--- src/main/groovy/com/bmuschko/gradle/docker/DockerExtension.groovy
+++ src/main/groovy/com/bmuschko/gradle/docker/DockerExtension.groovy
@@ -47,7 +47,13 @@ class DockerExtension {
                 dockerUrl = 'unix:///var/run/docker.sock'
             } else {
                 if (isWindows && new File("\\\\.\\pipe\\docker_engine").exists()) {
-                    dockerUrl = 'npipe:////./pipe/docker_engine'
+                    // TODO: re-enable once docker-java supports named pipes. Relevant links:
+                    // 
+                    //     https://github.com/bmuschko/gradle-docker-plugin/pull/313
+                    //     https://github.com/docker-java/docker-java/issues/765
+                    // 
+                    // dockerUrl = 'npipe:////./pipe/docker_engine'
+                    dockerUrl = 'tcp://127.0.0.1:2375'
                 } else {
                     dockerUrl = 'tcp://127.0.0.1:2375'
                 }
