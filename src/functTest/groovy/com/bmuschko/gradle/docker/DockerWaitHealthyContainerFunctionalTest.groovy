d88c7f80ecdd1675790cc5e4dbec202f1cbc7b6e
Gabriel Huff <gabrielhuff123@gmail.com>
2017-10-22 18:53:07 +0200

    Create successful execution test

--- src/functTest/groovy/com/bmuschko/gradle/docker/DockerWaitHealthyContainerFunctionalTest.groovy
+++ src/functTest/groovy/com/bmuschko/gradle/docker/DockerWaitHealthyContainerFunctionalTest.groovy
@@ -17,4 +17,41 @@ package com.bmuschko.gradle.docker
 
 class DockerWaitHealthyContainerFunctionalTest extends AbstractFunctionalTest {
 
+    def "Wait for a container to be healthy"() {
+        buildFile << """
+            import com.bmuschko.gradle.docker.tasks.image.*
+            import com.bmuschko.gradle.docker.tasks.container.*
+            import com.bmuschko.gradle.docker.tasks.container.extras.*
+
+            task dockerfile(type: Dockerfile) {
+                from '$TEST_IMAGE_WITH_TAG'
+                instruction { "HEALTHCHECK --interval=1s CMD test -e /tmp/HEALTHY || exit 1" }
+                defaultCommand '/bin/sh', '-c', 'sleep 5; touch /tmp/HEALTHY; sleep 60'
+            }
+
+            task buildImage(type: DockerBuildImage) {
+                dependsOn dockerfile
+                inputDir = file("build/docker")
+            }
+
+            task createContainer(type: DockerCreateContainer) {
+                dependsOn buildImage
+                targetImageId { buildImage.getImageId() }
+            }
+
+            task startContainer(type: DockerStartContainer) {
+                dependsOn createContainer
+                targetContainerId { createContainer.getContainerId() }
+            }
+
+            task waitContainer(type: DockerWaitHealthyContainer) {
+                dependsOn startContainer
+                targetContainerId { startContainer.getContainerId() }
+                timeout 10
+            }
+        """
+
+        expect:
+        build('waitContainer')
+    }
 }
