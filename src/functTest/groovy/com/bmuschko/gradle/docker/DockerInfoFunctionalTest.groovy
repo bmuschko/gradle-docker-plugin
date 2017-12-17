b0cf80d4ce1e8a5cda0eb31b37ba7c1b3361d8ce
Jonathan Springer <jonpspri@gmail.com>
2017-12-16 21:50:53 -0500

    Add JSON output option to DockerInfo. (#506)
    * Add JSON output option to DockerInfo.
    
    * Change approach to use onNext
    
    * Change approach to use onNext
    
    * Move logger and functional test.
    
    * Combine DockerInfo functional tests

--- src/functTest/groovy/com/bmuschko/gradle/docker/DockerInfoFunctionalTest.groovy
+++ src/functTest/groovy/com/bmuschko/gradle/docker/DockerInfoFunctionalTest.groovy
@@ -17,4 +17,27 @@ class DockerInfoFunctionalTest extends AbstractFunctionalTest {
         then:
         result.output.contains('Retrieving Docker info.')
     }
+
+    def "Passes dockerinfo to onNext if provided, w/o logger output"() {
+        buildFile << """
+            import com.bmuschko.gradle.docker.tasks.DockerInfo
+
+            task dockerInfo(type: DockerInfo) {
+                onNext { m ->
+                    if (m.architecture) {  // Because I implemented and I care
+                        logger.quiet "Architecture found"
+                    }
+                }
+            }
+        """
+
+        when:
+        BuildResult result = build('dockerInfo')
+
+        then:
+        (result.output.contains('Retrieving Docker info.')
+        && result.output.contains('Architecture found')
+        && !result.output.contains('Containers'))
+    }
+
 }
