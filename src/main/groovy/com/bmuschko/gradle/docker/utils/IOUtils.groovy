ad3eb2f81a189f9d62c080602a87d6269e671d68
orzeh <orzehoo@wp.pl>
2018-02-04 23:09:01 +0100

    Add DockerLoadImage and DockerSaveImage tasks (#531)
    * Add DockerLoadImage and DockerSaveImage tasks
    
    * Refine saving and loading images tests

---
+++ src/main/groovy/com/bmuschko/gradle/docker/utils/IOUtils.groovy
@@ -0,0 +1,26 @@
+package com.bmuschko.gradle.docker.utils
+
+final class IOUtils {
+
+    private static final int DEFAULT_BUFFER_SIZE = 1024 * 4
+
+    private IOUtils() {}
+
+    static void copy(InputStream input, OutputStream output) throws IOException {
+        byte[] buffer = new byte[DEFAULT_BUFFER_SIZE]
+        int n
+        while (-1 != (n = input.read(buffer))) {
+            output.write(buffer, 0, n)
+        }
+    }
+
+    static void closeQuietly(InputStream input) {
+        try {
+            if (input != null) {
+                input.close()
+            }
+        } catch (IOException ioe) {
+            // ignore
+        }
+    }
+}
