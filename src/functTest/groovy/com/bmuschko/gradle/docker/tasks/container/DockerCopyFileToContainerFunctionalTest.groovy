fd867dadf99ee75a86992986f1dab265b28cbf0b
Benjamin Muschko <benjamin.muschko@gmail.com>
2018-08-02 18:07:06 -0600

    Properly clean up stopped container and test code

--- src/functTest/groovy/com/bmuschko/gradle/docker/tasks/container/DockerCopyFileToContainerFunctionalTest.groovy
+++ src/functTest/groovy/com/bmuschko/gradle/docker/tasks/container/DockerCopyFileToContainerFunctionalTest.groovy
@@ -16,263 +16,151 @@
 package com.bmuschko.gradle.docker.tasks.container
 
 import com.bmuschko.gradle.docker.AbstractFunctionalTest
+import org.gradle.testkit.runner.BuildResult
+import org.gradle.testkit.runner.TaskOutcome
 
 class DockerCopyFileToContainerFunctionalTest extends AbstractFunctionalTest {
 
-    def "Copy file into container"() {
-
-        new File("$projectDir/HelloWorld.txt").withWriter('UTF-8') {
-            it.write('Hello, World!')
-        }
-
+    def "can copy a file into a container"() {
+        given:
+        writeHelloWorldFile()
+        buildFile << commonBuildScriptTasks()
         buildFile << """
-            import com.bmuschko.gradle.docker.tasks.image.DockerPullImage
-            import com.bmuschko.gradle.docker.tasks.container.DockerCreateContainer
-            import com.bmuschko.gradle.docker.tasks.container.DockerStartContainer
-            import com.bmuschko.gradle.docker.tasks.container.DockerExecContainer
-            import com.bmuschko.gradle.docker.tasks.container.DockerCopyFileToContainer
-            import com.bmuschko.gradle.docker.tasks.container.DockerRemoveContainer
-
-            task pullImage(type: DockerPullImage) {
-                repository = '$AbstractFunctionalTest.TEST_IMAGE'
-                tag = '$AbstractFunctionalTest.TEST_IMAGE_TAG'
-            }
-
-            task createContainer(type: DockerCreateContainer) {
-                dependsOn pullImage
-                targetImageId { pullImage.getImageId() }
-                cmd = ['echo', 'Hello World']
-            }
-
             task copyFileIntoContainer(type: DockerCopyFileToContainer) {
                 dependsOn createContainer
+                finalizedBy removeContainer
                 targetContainerId { createContainer.getContainerId() }
                 hostPath = "$projectDir/HelloWorld.txt"
                 remotePath = "/root"
             }
-            
-            task removeContainer(type: DockerRemoveContainer) {
-                dependsOn copyFileIntoContainer
-                removeVolumes = true
-                force = true
-                targetContainerId { createContainer.getContainerId() }
-            }
-
-            task workflow {
-                dependsOn removeContainer
-            }
         """
 
         expect:
-        build('workflow')
+        build('copyFileIntoContainer')
     }
 
-    def "Multi Copy file into container"() {
-
-        new File("$projectDir/HelloWorld.txt").withWriter('UTF-8') {
-            it.write('Hello, World!')
-        }
-
+    def "can copy multiple files into a container"() {
+        given:
+        writeHelloWorldFile()
+        buildFile << commonBuildScriptTasks()
         buildFile << """
-            import com.bmuschko.gradle.docker.tasks.image.DockerPullImage
-            import com.bmuschko.gradle.docker.tasks.container.DockerCreateContainer
-            import com.bmuschko.gradle.docker.tasks.container.DockerStartContainer
-            import com.bmuschko.gradle.docker.tasks.container.DockerExecContainer
-            import com.bmuschko.gradle.docker.tasks.container.DockerCopyFileToContainer
-            import com.bmuschko.gradle.docker.tasks.container.DockerRemoveContainer
-
-            task pullImage(type: DockerPullImage) {
-                repository = '$AbstractFunctionalTest.TEST_IMAGE'
-                tag = '$AbstractFunctionalTest.TEST_IMAGE_TAG'
-            }
-
-            task createContainer(type: DockerCreateContainer) {
-                dependsOn pullImage
-                targetImageId { pullImage.getImageId() }
-                cmd = ['echo', 'Hello World']
-            }
-
             task copyFileIntoContainer(type: DockerCopyFileToContainer) {
                 dependsOn createContainer
+                finalizedBy removeContainer
                 targetContainerId { createContainer.getContainerId() }
                 withFile("$projectDir/HelloWorld.txt", '/root')
                 withFile("$projectDir/HelloWorld.txt", '/tmp')
                 withFile({ "$projectDir/HelloWorld.txt" }, { '/' })
             }
-            
-            task removeContainer(type: DockerRemoveContainer) {
-                dependsOn copyFileIntoContainer
-                removeVolumes = true
-                force = true
-                targetContainerId { createContainer.getContainerId() }
-            }
-
-            task workflow {
-                dependsOn removeContainer
-            }
         """
 
         expect:
-        build('workflow')
+        build('copyFileIntoContainer')
     }
 
-    def "Copy tarfile into container"() {
-
-        new File("$projectDir/HelloWorld.txt").withWriter('UTF-8') {
-            it.write('Hello, World!')
-        }
-
+    def "can copy a TAR file into a container"() {
+        given:
+        writeHelloWorldFile()
+        buildFile << commonBuildScriptTasks()
+        buildFile << tarTask()
         buildFile << """
-            import com.bmuschko.gradle.docker.tasks.image.DockerPullImage
-            import com.bmuschko.gradle.docker.tasks.container.DockerCreateContainer
-            import com.bmuschko.gradle.docker.tasks.container.DockerStartContainer
-            import com.bmuschko.gradle.docker.tasks.container.DockerExecContainer
-            import com.bmuschko.gradle.docker.tasks.container.DockerCopyFileToContainer
-            import com.bmuschko.gradle.docker.tasks.container.DockerRemoveContainer
-
-            task createTarFile(type: Tar) {
-                from "$projectDir/HelloWorld.txt"
-                baseName = 'HelloWorld'
-                destinationDir = projectDir
-                extension = 'tgz'
-                compression = Compression.GZIP
-            }
-			
-            task pullImage(type: DockerPullImage) {
-                dependsOn createTarFile
-                repository = '$AbstractFunctionalTest.TEST_IMAGE'
-                tag = '$AbstractFunctionalTest.TEST_IMAGE_TAG'
-            }
-
-            task createContainer(type: DockerCreateContainer) {
-                dependsOn pullImage
-                targetImageId { pullImage.getImageId() }
-                cmd = ['echo', 'Hello World']
-            }
-
             task copyFileIntoContainer(type: DockerCopyFileToContainer) {
-                dependsOn createContainer
+                dependsOn createContainer, createTarFile
+                finalizedBy removeContainer
                 targetContainerId { createContainer.getContainerId() }
                 tarFile { new File("$projectDir/HelloWorld.tgz") }
                 remotePath = "/root"
             }
-            
-            task removeContainer(type: DockerRemoveContainer) {
-                dependsOn copyFileIntoContainer
-                removeVolumes = true
-                force = true
-                targetContainerId { createContainer.getContainerId() }
-            }
-
-            task workflow {
-                dependsOn removeContainer
-            }
         """
 
         expect:
-        build('workflow')
+        build('copyFileIntoContainer')
     }
 
-    def "Multi copy tarfile into container"() {
-
-        new File("$projectDir/HelloWorld.txt").withWriter('UTF-8') {
-            it.write('Hello, World!')
-        }
-
+    def "can copy multiple TAR files into a container"() {
+        given:
+        writeHelloWorldFile()
+        buildFile << commonBuildScriptTasks()
+        buildFile << tarTask()
         buildFile << """
-            import com.bmuschko.gradle.docker.tasks.image.DockerPullImage
-            import com.bmuschko.gradle.docker.tasks.container.DockerCreateContainer
-            import com.bmuschko.gradle.docker.tasks.container.DockerStartContainer
-            import com.bmuschko.gradle.docker.tasks.container.DockerExecContainer
-            import com.bmuschko.gradle.docker.tasks.container.DockerCopyFileToContainer
-            import com.bmuschko.gradle.docker.tasks.container.DockerRemoveContainer
-
-            task createTarFile(type: Tar) {
-                from "$projectDir/HelloWorld.txt"
-                baseName = 'HelloWorld'
-                destinationDir = projectDir
-                extension = 'tgz'
-                compression = Compression.GZIP
-            }
-			
-            task pullImage(type: DockerPullImage) {
-                dependsOn createTarFile
-                repository = '$AbstractFunctionalTest.TEST_IMAGE'
-                tag = '$AbstractFunctionalTest.TEST_IMAGE_TAG'
-            }
-
-            task createContainer(type: DockerCreateContainer) {
-                dependsOn pullImage
-                targetImageId { pullImage.getImageId() }
-                cmd = ['echo', 'Hello World']
-            }
-
             task copyFileIntoContainer(type: DockerCopyFileToContainer) {
-                dependsOn createContainer
+                dependsOn createContainer, createTarFile
+                finalizedBy removeContainer
                 targetContainerId { createContainer.getContainerId() }
                 withTarFile({ new File("$projectDir/HelloWorld.tgz") }, '/root')
                 withTarFile({ new File("$projectDir/HelloWorld.tgz") }, {'/'} )
             }
-            
-            task removeContainer(type: DockerRemoveContainer) {
-                dependsOn copyFileIntoContainer
-                removeVolumes = true
-                force = true
-                targetContainerId { createContainer.getContainerId() }
-            }
-
-            task workflow {
-                dependsOn removeContainer
-            }
         """
 
         expect:
-        build('workflow')
+        build('copyFileIntoContainer')
     }
 
-    def "Fail if both hostPath and tarFile are specified"() {
-
-        new File("$projectDir/HelloWorld.txt").withWriter('UTF-8') {
-            it.write('Hello, World!')
-        }
-
+    def "fails task execution if properties hostPath and tarFile are both specified"() {
+        given:
+        writeHelloWorldFile()
+        buildFile << commonBuildScriptTasks()
         buildFile << """
-            import com.bmuschko.gradle.docker.tasks.image.DockerPullImage
-            import com.bmuschko.gradle.docker.tasks.container.DockerCreateContainer
-            import com.bmuschko.gradle.docker.tasks.container.DockerStartContainer
-            import com.bmuschko.gradle.docker.tasks.container.DockerExecContainer
-            import com.bmuschko.gradle.docker.tasks.container.DockerCopyFileToContainer
-            import com.bmuschko.gradle.docker.tasks.container.DockerRemoveContainer
-
-            task pullImage(type: DockerPullImage) {
-                repository = '$AbstractFunctionalTest.TEST_IMAGE'
-                tag = '$AbstractFunctionalTest.TEST_IMAGE_TAG'
-            }
-
-            task createContainer(type: DockerCreateContainer) {
-                dependsOn pullImage
-                targetImageId { pullImage.getImageId() }
-                cmd = ['echo', 'Hello World']
-            }
-
             task copyFileIntoContainer(type: DockerCopyFileToContainer) {
                 dependsOn createContainer
+                finalizedBy removeContainer
                 targetContainerId { createContainer.getContainerId() }
                 hostPath = "$projectDir/HelloWorld.txt"
                 tarFile { new File("$projectDir/HelloWorld.txt") }
                 remotePath = "/root"
             }
-
-            task workflow {
-                dependsOn copyFileIntoContainer
-            }
         """
 
         when:
-            build('workflow')
+        BuildResult result = buildAndFail('copyFileIntoContainer')
 
         then:
-            Exception ex = thrown()
-            ex.message.contains('Can specify either hostPath or tarFile not both')
+        result.task(':copyFileIntoContainer').outcome == TaskOutcome.FAILED
+        result.output.contains('Can specify either hostPath or tarFile not both')
+    }
+
+    private void writeHelloWorldFile() {
+        new File("$projectDir/HelloWorld.txt").withWriter('UTF-8') {
+            it.write('Hello, World!')
+        }
+    }
+
+    static String commonBuildScriptTasks() {
+        """
+            import com.bmuschko.gradle.docker.tasks.image.DockerPullImage
+            import com.bmuschko.gradle.docker.tasks.container.DockerCreateContainer
+            import com.bmuschko.gradle.docker.tasks.container.DockerStartContainer
+            import com.bmuschko.gradle.docker.tasks.container.DockerCopyFileToContainer
+            import com.bmuschko.gradle.docker.tasks.container.DockerRemoveContainer
+
+            task pullImage(type: DockerPullImage) {
+                repository = '$AbstractFunctionalTest.TEST_IMAGE'
+                tag = '$AbstractFunctionalTest.TEST_IMAGE_TAG'
+            }
+
+            task createContainer(type: DockerCreateContainer) {
+                dependsOn pullImage
+                targetImageId { pullImage.getImageId() }
+                cmd = ['echo', 'Hello World']
+            }
+            
+            task removeContainer(type: DockerRemoveContainer) {
+                removeVolumes = true
+                force = true
+                targetContainerId { createContainer.getContainerId() }
+            }
+        """
+    }
+
+    private String tarTask() {
+        """
+            task createTarFile(type: Tar) {
+                from "$projectDir/HelloWorld.txt"
+                baseName = 'HelloWorld'
+                destinationDir = projectDir
+                extension = 'tgz'
+                compression = Compression.GZIP
+            }
+        """
     }
 }
