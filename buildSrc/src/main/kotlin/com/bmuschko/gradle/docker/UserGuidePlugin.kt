b5fd8908a7b56592952d60386db8a4650d5c40fa
Benjamin Muschko <benjamin.muschko@gmail.com>
2018-10-20 16:30:46 -0600

    Use the same version of AsciidoctorJ to avoid classpath issues

--- buildSrc/src/main/kotlin/com/bmuschko/gradle/docker/UserGuidePlugin.kt
+++ buildSrc/src/main/kotlin/com/bmuschko/gradle/docker/UserGuidePlugin.kt
@@ -1,23 +1,33 @@
 package com.bmuschko.gradle.docker
 
+import org.asciidoctor.gradle.AsciidoctorExtension
 import org.asciidoctor.gradle.AsciidoctorPlugin
 import org.asciidoctor.gradle.AsciidoctorTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.tasks.util.PatternSet
 import org.gradle.kotlin.dsl.apply
+import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.delegateClosureOf
 import org.gradle.kotlin.dsl.named
 
 class UserGuidePlugin : Plugin<Project> {
     override fun apply(project: Project): Unit = project.run {
         applyAsciidocPlugin()
+        configureAsciidoctorExtension()
         configureAsciidoctorTask()
     }
 
     private
     fun Project.applyAsciidocPlugin() {
         apply<AsciidoctorPlugin>()
+    }
+
+    private
+    fun Project.configureAsciidoctorExtension() {
+        configure<AsciidoctorExtension> {
+            setVersion("1.6.0-alpha.7")
+        }
     }
 
     private
