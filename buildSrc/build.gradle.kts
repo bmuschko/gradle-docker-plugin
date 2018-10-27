5bc87d069db04034292d3c37c3fbf31521b54917
Benjamin Muschko <benjamin.muschko@gmail.com>
2018-10-27 12:04:40 -0600

    Upgrade to latest version
    Fixes bug that caused an AsciidoctorJ extension to be run only once.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,7 +10,7 @@ repositories {
 dependencies {
     implementation(kotlin("gradle-plugin"))
     implementation("org.asciidoctor:asciidoctorj:1.6.0-alpha.7")
-    implementation("org.asciidoctor:asciidoctor-gradle-plugin:1.5.3")
+    implementation("org.asciidoctor:asciidoctor-gradle-plugin:1.5.9.1")
     implementation("org.ajoberstar:grgit:1.9.1") {
         setForce(true)
     }
