e115d8aad3d2520a93068a567ed9d49ba276123e
Clayton Walker <cwalker@sofi.org>
2022-11-28 00:06:02 -0700

    Fixups in dockerfile

--- src/main/groovy/com/bmuschko/gradle/docker/tasks/image/Dockerfile.java
+++ src/main/groovy/com/bmuschko/gradle/docker/tasks/image/Dockerfile.java
@@ -1,8 +1,20 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.bmuschko.gradle.docker.tasks.image;
 
-import groovy.lang.Closure;
-import groovy.lang.GString;
-import groovy.lang.Reference;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.Transformer;
 import org.gradle.api.file.Directory;
@@ -22,14 +34,12 @@ import org.gradle.api.tasks.OutputFile;
 import org.gradle.api.tasks.TaskAction;
 
 import javax.annotation.Nullable;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
