7693c9cb2d14cd45bc44aafea3e204da0f1d98a7
Clayton Walker <clayton.m.walker@gmail.com>
2022-12-18 18:52:01 -0700

    Convert config to java

--- src/main/groovy/com/bmuschko/gradle/docker/tasks/DockerClientConfiguration.java
+++ src/main/groovy/com/bmuschko/gradle/docker/tasks/DockerClientConfiguration.java
@@ -1,56 +1,54 @@
-package com.bmuschko.gradle.docker.tasks
+package com.bmuschko.gradle.docker.tasks;
 
-import groovy.transform.CompileStatic
-import org.gradle.api.file.Directory
+import org.gradle.api.file.Directory;
 
-@CompileStatic
-class DockerClientConfiguration {
+public class DockerClientConfiguration {
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Directory getCertPath() {
+        return certPath;
+    }
+
+    public void setCertPath(Directory certPath) {
+        this.certPath = certPath;
+    }
+
+    public String getApiVersion() {
+        return apiVersion;
+    }
+
+    public void setApiVersion(String apiVersion) {
+        this.apiVersion = apiVersion;
+    }
+
     private String url;
     private Directory certPath;
     private String apiVersion;
 
-    String getUrl() {
-        return url
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || !getClass().equals(o.getClass())) return false;
+
+        DockerClientConfiguration that = (DockerClientConfiguration) o;
+
+        if (!apiVersion.equals(that.getApiVersion())) return false;
+        if (!certPath.equals(that.getCertPath())) return false;
+        if (!url.equals(that.getUrl())) return false;
+
+        return true;
     }
 
-    void setUrl(String url) {
-        this.url = url
-    }
-
-    Directory getCertPath() {
-        return certPath
-    }
-
-    void setCertPath(Directory certPath) {
-        this.certPath = certPath
-    }
-
-    String getApiVersion() {
-        return apiVersion
-    }
-
-    void setApiVersion(String apiVersion) {
-        this.apiVersion = apiVersion
-    }
-
-    boolean equals(o) {
-        if (this.is(o)) return true
-        if (o == null || getClass() != o.class) return false
-
-        DockerClientConfiguration that = (DockerClientConfiguration) o
-
-        if (apiVersion != that.apiVersion) return false
-        if (certPath != that.certPath) return false
-        if (url != that.url) return false
-
-        return true
-    }
-
-    int hashCode() {
-        int result
-        result = (url != null ? url.hashCode() : 0)
-        result = 31 * result + (certPath != null ? certPath.hashCode() : 0)
-        result = 31 * result + (apiVersion != null ? apiVersion.hashCode() : 0)
-        return result
+    public int hashCode() {
+        int result;
+        result = (url != null ? url.hashCode() : 0);
+        result = 31 * result + (certPath != null ? certPath.hashCode() : 0);
+        result = 31 * result + (apiVersion != null ? apiVersion.hashCode() : 0);
+        return result;
     }
 }
