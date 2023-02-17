af8f387a345bd375af8f29e1f9c1b1c691e901d0
Benjamin Muschko <benjamin.muschko@gmail.com>
2023-02-17 10:47:34 -0700

    Introduce property for setting a USER instruction
    For security reasons, you will want to run the container with a different user than "root". Provides a property for setting the user.

--- src/main/java/com/bmuschko/gradle/docker/DockerConventionJvmApplicationPlugin.java
+++ src/main/java/com/bmuschko/gradle/docker/DockerConventionJvmApplicationPlugin.java
@@ -4,7 +4,6 @@ import com.bmuschko.gradle.docker.internal.ConventionPluginHelper;
 import com.bmuschko.gradle.docker.tasks.image.DockerBuildImage;
 import com.bmuschko.gradle.docker.tasks.image.DockerPushImage;
 import com.bmuschko.gradle.docker.tasks.image.Dockerfile;
-import org.gradle.api.Action;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.model.ObjectFactory;
@@ -15,8 +14,11 @@ import org.gradle.api.tasks.TaskProvider;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.*;
-import java.util.concurrent.Callable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import static com.bmuschko.gradle.docker.internal.ConventionPluginHelper.createAppFilesCopySpec;
 
@@ -53,140 +55,95 @@ public abstract class DockerConventionJvmApplicationPlugin<EXT extends DockerCon
         project.getPlugins().withType(JavaPlugin.class).configureEach(javaPlugin -> {
             TaskProvider<Dockerfile> createDockerfileTask = registerDockerfileTask(project, extension);
             final TaskProvider<Sync> syncBuildContextTask = registerSyncBuildContextTask(project, createDockerfileTask);
-            createDockerfileTask.configure(new Action<Dockerfile>() {
-                @Override
-                public void execute(Dockerfile dockerfile) {
-                    dockerfile.dependsOn(syncBuildContextTask);
-                }
-            });
+            createDockerfileTask.configure(dockerfile -> dockerfile.dependsOn(syncBuildContextTask));
             TaskProvider<DockerBuildImage> dockerBuildImageTask = registerBuildImageTask(project, createDockerfileTask, extension);
             registerPushImageTask(project, dockerBuildImageTask);
         });
     }
 
     private TaskProvider<Dockerfile> registerDockerfileTask(final Project project, final EXT extension) {
-        return project.getTasks().register(DOCKERFILE_TASK_NAME, Dockerfile.class, new Action<Dockerfile>() {
-            @Override
-            public void execute(final Dockerfile dockerfile) {
-                dockerfile.setGroup(DockerRemoteApiPlugin.DEFAULT_TASK_GROUP);
-                dockerfile.setDescription("Creates the Docker image for the application.");
-                dockerfile.from(project.provider(new Callable<Dockerfile.From>() {
-                    @Override
-                    public Dockerfile.From call() throws Exception {
-                        return new Dockerfile.From(extension.getBaseImage().get());
-                    }
+        return project.getTasks().register(DOCKERFILE_TASK_NAME, Dockerfile.class, dockerfile -> {
+            dockerfile.setGroup(DockerRemoteApiPlugin.DEFAULT_TASK_GROUP);
+            dockerfile.setDescription("Creates the Docker image for the application.");
+            dockerfile.from(project.provider(() -> new Dockerfile.From(extension.getBaseImage().get())));
+            dockerfile.label(project.provider(() -> new HashMap<>(Map.ofEntries(Map.entry("maintainer", extension.getMaintainer().get())))));
+            dockerfile.user(extension.getUser());
+            dockerfile.workingDir("/app");
+            dockerfile.copyFile(project.provider(() -> {
+                if (new File(dockerfile.getDestDir().get().getAsFile(), "libs").isDirectory()) {
+                    return new Dockerfile.CopyFile("libs", "libs/");
+                }
 
-                }));
-                dockerfile.label(project.provider(new Callable<Map<String, String>>() {
-                    @Override
-                    public Map<String, String> call() throws Exception {
-                        return new HashMap<>(Map.ofEntries(Map.entry("maintainer", extension.getMaintainer().get())));
-                    }
+                return null;
+            }));
+            dockerfile.copyFile(project.provider(() -> {
+                if (ConventionPluginHelper.getMainJavaSourceSetOutput(project).getResourcesDir().isDirectory()) {
+                    return new Dockerfile.CopyFile("resources", "resources/");
+                }
 
-                }));
-                dockerfile.workingDir("/app");
-                dockerfile.copyFile(project.provider(new Callable<Dockerfile.CopyFile>() {
-                    @Override
-                    public Dockerfile.CopyFile call() throws Exception {
-                        if (new File(dockerfile.getDestDir().get().getAsFile(), "libs").isDirectory()) {
-                            return new Dockerfile.CopyFile("libs", "libs/");
-                        }
+                return null;
+            }));
+            dockerfile.copyFile(new Dockerfile.CopyFile("classes", "classes/"));
+            dockerfile.entryPoint(project.provider(() -> {
+                List<String> entrypoint = new ArrayList<>(List.of("java"));
+                List<String> jvmArgs = extension.getJvmArgs().get();
 
-                        return null;
-                    }
+                if (!jvmArgs.isEmpty()) {
+                    entrypoint.addAll(jvmArgs);
+                }
 
-                }));
-                dockerfile.copyFile(project.provider(new Callable<Dockerfile.CopyFile>() {
-                    @Override
-                    public Dockerfile.CopyFile call() throws Exception {
-                        if (ConventionPluginHelper.getMainJavaSourceSetOutput(project).getResourcesDir().isDirectory()) {
-                            return new Dockerfile.CopyFile("resources", "resources/");
-                        }
+                entrypoint.addAll(List.of("-cp", "/app/resources:/app/classes:/app/libs/*", getApplicationMainClassName(project, extension)));
 
-                        return null;
-                    }
+                List<String> args = extension.getArgs().get();
 
-                }));
-                dockerfile.copyFile(new Dockerfile.CopyFile("classes", "classes/"));
-                dockerfile.entryPoint(project.provider(new Callable<List<String>>() {
-                    @Override
-                    public List<String> call() throws Exception {
-                        List<String> entrypoint = new ArrayList<>(List.of("java"));
-                        List<String> jvmArgs = extension.getJvmArgs().get();
-
-                        if (!jvmArgs.isEmpty()) {
-                            entrypoint.addAll(jvmArgs);
-                        }
-
-                        entrypoint.addAll(List.of("-cp", "/app/resources:/app/classes:/app/libs/*", getApplicationMainClassName(project, extension)));
-
-                        List<String> args = extension.getArgs().get();
-
-                        if (!args.isEmpty()) {
-                            entrypoint.addAll(args);
-                        }
+                if (!args.isEmpty()) {
+                    entrypoint.addAll(args);
+                }
 
 
-                        return entrypoint;
-                    }
-
-                }));
-                dockerfile.exposePort(extension.getPorts());
-            }
-
+                return entrypoint;
+            }));
+            dockerfile.exposePort(extension.getPorts());
         });
     }
 
     private static TaskProvider<Sync> registerSyncBuildContextTask(final Project project, final TaskProvider<Dockerfile> createDockerfileTask) {
-        return project.getTasks().register(SYNC_BUILD_CONTEXT_TASK_NAME, Sync.class, new Action<Sync>() {
-            @Override
-            public void execute(Sync sync) {
-                sync.setGroup(DockerRemoteApiPlugin.DEFAULT_TASK_GROUP);
-                sync.setDescription("Copies the distribution resources to a temporary directory for image creation.");
-                sync.dependsOn(project.getTasks().getByName(JavaPlugin.CLASSES_TASK_NAME));
-                sync.into(createDockerfileTask.get().getDestDir());
-                sync.with(createAppFilesCopySpec(project));
-            }
+        return project.getTasks().register(SYNC_BUILD_CONTEXT_TASK_NAME, Sync.class, sync -> {
+            sync.setGroup(DockerRemoteApiPlugin.DEFAULT_TASK_GROUP);
+            sync.setDescription("Copies the distribution resources to a temporary directory for image creation.");
+            sync.dependsOn(project.getTasks().getByName(JavaPlugin.CLASSES_TASK_NAME));
+            sync.into(createDockerfileTask.get().getDestDir());
+            sync.with(createAppFilesCopySpec(project));
         });
     }
 
     private TaskProvider<DockerBuildImage> registerBuildImageTask(final Project project, final TaskProvider<Dockerfile> createDockerfileTask, final EXT extension) {
-        return project.getTasks().register(BUILD_IMAGE_TASK_NAME, DockerBuildImage.class, new Action<DockerBuildImage>() {
-            @Override
-            public void execute(DockerBuildImage dockerBuildImage) {
-                dockerBuildImage.setGroup(DockerRemoteApiPlugin.DEFAULT_TASK_GROUP);
-                dockerBuildImage.setDescription("Builds the Docker image for the application.");
-                dockerBuildImage.dependsOn(createDockerfileTask);
-                dockerBuildImage.getImages().addAll(determineImages(project, extension));
-            }
+        return project.getTasks().register(BUILD_IMAGE_TASK_NAME, DockerBuildImage.class, dockerBuildImage -> {
+            dockerBuildImage.setGroup(DockerRemoteApiPlugin.DEFAULT_TASK_GROUP);
+            dockerBuildImage.setDescription("Builds the Docker image for the application.");
+            dockerBuildImage.dependsOn(createDockerfileTask);
+            dockerBuildImage.getImages().addAll(determineImages(project, extension));
         });
     }
 
     private Provider<Set<String>> determineImages(final Project project, final EXT extension) {
-        return project.provider(new Callable<Set<String>>() {
-            @Override
-            public Set<String> call() throws Exception {
-                if (extension.getImages().getOrNull() != null && !extension.getImages().get().isEmpty()) {
-                    return extension.getImages().get();
-                }
-
-                final String tagVersion = project.getVersion().equals("unspecified") ? "latest" : project.getVersion().toString();
-                String artifactAndVersion = (project.getName() + ":" + tagVersion).toLowerCase();
-                return Set.of(!project.getGroup().toString().isEmpty() ? project.getGroup() + "/" + artifactAndVersion : artifactAndVersion);
+        return project.provider(() -> {
+            if (extension.getImages().getOrNull() != null && !extension.getImages().get().isEmpty()) {
+                return extension.getImages().get();
             }
 
+            final String tagVersion = project.getVersion().equals("unspecified") ? "latest" : project.getVersion().toString();
+            String artifactAndVersion = (project.getName() + ":" + tagVersion).toLowerCase();
+            return Set.of(!project.getGroup().toString().isEmpty() ? project.getGroup() + "/" + artifactAndVersion : artifactAndVersion);
         });
     }
 
     private static TaskProvider<DockerPushImage> registerPushImageTask(Project project, final TaskProvider<DockerBuildImage> dockerBuildImageTask) {
-        return project.getTasks().register(PUSH_IMAGE_TASK_NAME, DockerPushImage.class, new Action<DockerPushImage>() {
-            @Override
-            public void execute(DockerPushImage pushImage) {
-                pushImage.setGroup(DockerRemoteApiPlugin.DEFAULT_TASK_GROUP);
-                pushImage.setDescription("Pushes created Docker image to the repository.");
-                pushImage.dependsOn(dockerBuildImageTask);
-                pushImage.getImages().convention(dockerBuildImageTask.get().getImages());
-            }
+        return project.getTasks().register(PUSH_IMAGE_TASK_NAME, DockerPushImage.class, pushImage -> {
+            pushImage.setGroup(DockerRemoteApiPlugin.DEFAULT_TASK_GROUP);
+            pushImage.setDescription("Pushes created Docker image to the repository.");
+            pushImage.dependsOn(dockerBuildImageTask);
+            pushImage.getImages().convention(dockerBuildImageTask.get().getImages());
         });
     }
 
@@ -209,6 +166,4 @@ public abstract class DockerConventionJvmApplicationPlugin<EXT extends DockerCon
     protected abstract EXT configureExtension(ObjectFactory objectFactory, DockerExtension dockerExtension);
 
     protected abstract String findMainClassName(File classesDir) throws IOException;
-
-
 }
